ef7dc24ff3b62ad6951c89a93a632e750886111e
alperozturk <alper_ozturk@proton.me>
2023-12-26 13:57:47 +0100

    Make getServerResponse nullable
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- library/src/main/java/com/owncloud/android/lib/resources/OCSRemoteOperation.kt
+++ library/src/main/java/com/owncloud/android/lib/resources/OCSRemoteOperation.kt
@@ -24,45 +24,28 @@
  *   THE SOFTWARE.
  *
  */
+package com.owncloud.android.lib.resources
 
-package com.owncloud.android.lib.resources;
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
+import com.nextcloud.common.OkHttpMethodBase
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import org.apache.commons.httpclient.HttpMethodBase
+import java.io.IOException
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import com.nextcloud.common.OkHttpMethodBase;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
+abstract class OCSRemoteOperation<T> : RemoteOperation<T>() {
+    private val gson = Gson()
 
-import org.apache.commons.httpclient.HttpMethodBase;
-
-import java.io.IOException;
-
-/**
- * Base class for OCS remote operations with convenient methods
- *
- * @author Bartosz Przybylski
- */
-public abstract class OCSRemoteOperation<T> extends RemoteOperation<T> {
-
-    @Deprecated
-    public <T> T getServerResponse(HttpMethodBase method, TypeToken<T> type) throws IOException {
-        String response = method.getResponseBodyAsString();
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(response);
-
-        Gson gson = new Gson();
-
-        return gson.fromJson(element, type.getType());
+    fun <T> getServerResponse(method: HttpMethodBase, type: TypeToken<T>): T? {
+        val response = method.responseBodyAsString
+        val element = JsonParser.parseString(response).asJsonObject
+        return gson.fromJson(element, type.type)
     }
 
-    public <T> T getServerResponse(OkHttpMethodBase method, TypeToken<T> type) {
-        String response = method.getResponseBodyAsString();
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(response);
-
-        Gson gson = new Gson();
-
-        return gson.fromJson(element, type.getType());
+    fun <T> getServerResponse(method: OkHttpMethodBase, type: TypeToken<T>): T? {
+        val response = method.getResponseBodyAsString()
+        val element = JsonParser.parseString(response).asJsonObject
+        return gson.fromJson(element, type.type)
     }
 }
